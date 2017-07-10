a16f9c239eebf513731fc02615c4e114469096a7
李闯 <lichuang@baijiahulian.com>
2017-07-10 22:44:31 +0800

    add shareditor and web dir

---
+++ shareditor/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for shareditor project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shareditor.settings")
+
+application = get_wsgi_application()
