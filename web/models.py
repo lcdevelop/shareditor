34d2f78bff2a3ff20ff52c8991beee863ae23a58
李闯 <lichuang@baijiahulian.com>
2017-07-11 09:26:20 +0800

    add some models

--- web/models.py
+++ web/models.py
@@ -3,4 +3,22 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Subject(models.Model):
+    name = models.CharField(max_length=255, verbose_name='类别名称')
+    introduce = models.CharField(max_length=255, verbose_name='类别简介')
+    image = models.ImageField(verbose_name='类别图片')
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, verbose_name='标签名称')
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=255, verbose_name='文章标题')
+    body = models.TextField(verbose_name='文章内容')
+    create_time = models.DateTimeField(verbose_name='创建时间')
+    subject = models.ForeignKey(Subject, verbose_name='类别', null=True)
+    tags = models.ManyToManyField(Tag, verbose_name='标签')
+
+
--- web/views.py
+++ web/views.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import BlogPost, Subject
 
 
 def index(request):
+    BlogPost.objects.filter(tag)
     return HttpResponse('Hello World!')
