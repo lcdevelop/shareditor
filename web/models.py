e4f1d488900383e42615d10f4553773708545550
李闯 <lichuang@baijiahulian.com>
2017-07-19 14:55:51 +0800

    blog show

--- web/models.py
+++ web/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class Subject(models.Model):
     name = models.CharField(max_length=255, verbose_name='类别名称')
     introduce = models.CharField(max_length=255, verbose_name='类别简介')
-    image = models.ImageField(verbose_name='类别图片', null=True)
+    image = models.ImageField(max_length=255, verbose_name='类别图片', null=True)
 
     class Meta:
         verbose_name_plural = '类别'
@@ -18,7 +18,10 @@ class Subject(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=255, verbose_name='标签名称')
-    image = models.ImageField(verbose_name='标签图片', null=True)
+    image = models.ImageField(max_length=255, verbose_name='标签图片', null=True)
+
+    def get_latest_blogpost(self, count=5):
+        return self.blogpost_set.order_by('id').reverse()[0:count]
 
     class Meta:
         verbose_name_plural = '标签'
@@ -30,13 +33,16 @@ class Tag(models.Model):
 class BlogPost(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255, verbose_name='文章标题')
-    image = models.ImageField(verbose_name='文章图片', null=True)
+    image = models.ImageField(max_length=255, verbose_name='文章图片', null=True)
     abstract = models.CharField(max_length=255, verbose_name='文章摘要', null=True)
     body = models.TextField(verbose_name='文章内容')
     create_time = models.DateTimeField(verbose_name='创建时间')
     subject = models.ForeignKey(Subject, verbose_name='类别', null=True)
     tags = models.ManyToManyField(Tag, verbose_name='标签', null=True)
     pv = models.IntegerField(verbose_name='pv', default=0)
+
+    def get_simple_title(self):
+        return self.title.replace(self.tags.first().name, '')
 
     class Meta:
         verbose_name_plural = '文章'
--- web/views.py
+++ web/views.py
@@ -8,8 +8,10 @@ from models import BlogPost, Subject, Tag
 
 def index(request):
     tags = Tag.objects.all()
-    latest_blog_posts = BlogPost.objects.order_by('create_time')[0:5]
-    return render(request, 'web/index.html', {'tags': tags, 'latest_blog_posts': latest_blog_posts})
+    latest_blog_posts = BlogPost.objects.order_by('create_time').reverse()[0:5]
+    hottest_blog_posts = BlogPost.objects.order_by('pv').reverse()[0:5]
+    return render(request, 'web/index.html', {'tags': tags, 'latest_blog_posts': latest_blog_posts,
+                                              'hottest_blog_posts': hottest_blog_posts})
 
 
 def blog_list_by_tag(request):
@@ -17,8 +19,10 @@ def blog_list_by_tag(request):
         tag_name = request.GET['tagname']
         blog_posts = BlogPost.objects.filter(tags__name=tag_name)
         latest_blog_posts = BlogPost.objects.order_by('create_time')[0:5]
+        hottest_blog_posts = BlogPost.objects.order_by('pv').reverse()[0:5]
         return render(request, 'web/blog_list_by_tag.html', {'tag_name': tag_name, 'blog_posts': blog_posts,
-                                                             'latest_blog_posts': latest_blog_posts})
+                                                             'latest_blog_posts': latest_blog_posts,
+                                                             'hottest_blog_posts': hottest_blog_posts})
     else:
         return HttpResponse('404')
 
@@ -28,11 +32,14 @@ def blog_show(request):
         blog_id = request.GET['blogId']
         blog_post = BlogPost.objects.get(id=blog_id)
         latest_blog_posts = BlogPost.objects.order_by('create_time')[0:5]
+        hottest_blog_posts = BlogPost.objects.order_by('pv').reverse()[0:5]
         tag_blog_posts = BlogPost.objects.filter(tags__name=blog_post.tags.first())
         tags = Tag.objects.all()
-        prev_blog_post = BlogPost.objects.filter(tags__name=blog_post.tags.first()).filter(id__gt=blog_post.id).order_by('id')[0:1]
-        next_blog_post = BlogPost.objects.filter(tags__name=blog_post.tags.first()).filter(id__gt=blog_post.id).order_by('id').reverse()[0:1]
-        return render(request, 'web/blog_show.html', {'blog_post': blog_post, 'latest_blog_posts': latest_blog_posts,
+        prev_blog_post = BlogPost.objects.filter(tags__name=blog_post.tags.first()).filter(id__lt=blog_post.id).order_by('id').reverse()[0:1].first()
+        next_blog_post = BlogPost.objects.filter(tags__name=blog_post.tags.first()).filter(id__gt=blog_post.id).order_by('id')[0:1].first()
+        return render(request, 'web/blog_show.html', {'blog_post': blog_post,
+                                                      'latest_blog_posts': latest_blog_posts,
+                                                      'hottest_blog_posts': hottest_blog_posts,
                                                       'tag_blog_posts': tag_blog_posts, 'tags': tags,
                                                       'prev_blog_post': prev_blog_post, 'next_blog_post': next_blog_post})
     else:
