d3f32e96e2e2109487e2f458c68191fd0699b25d
李闯 <lichuang@baijiahulian.com>
2017-08-15 11:28:34 +0800

    baidu_union verify

--- shareditor/urls.py
+++ shareditor/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     url(r'^chatbot/$', views.chatbot, name='chatbot'),
     url(r'^chatbot/query', views.chatbot_query, name='chatbot'),
     url(r'^robots.txt$', TemplateView.as_view(template_name='static/robots.txt')),
+    url(r'^bdunion.txt$', TemplateView.as_view(template_name='static/bdunion.txt')),
     url(r'^report/pv.gif$', views.report_pv, name='report_pv'),
 ]
 
